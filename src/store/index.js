8595610fd4e4f4ebee18dbb0431896ac82f582be
Viktor Abakumov <hello@imatte.ru>
2022-09-06 11:21:24 +0500

    Types

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import { createStore } from "vuex";
 import { API_DEVELOPMENTS_URL } from "@/common/config.js";
+import { FETCH_DEVS } from "./actions.type";
+import { SET_DEVS } from "./mutations.type";
 
 const state = {
   devs: [],
@@ -10,13 +12,13 @@ const getters = {
 };
 
 const mutations = {
-  setDevs(state, payload) {
+  [SET_DEVS](state, payload) {
     state.devs = payload;
   },
 };
 
 const actions = {
-  async fetchDevs({ commit }) {
+  async [FETCH_DEVS]({ commit }) {
     const data = await (await fetch(API_DEVELOPMENTS_URL)).json();
     commit("setDevs", data);
   },
